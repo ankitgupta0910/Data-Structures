3b713aee71c5e65849c3b9b2274a9b28f37e5358
Ankit Gupta <ankitgupta09101987@gmail.com>
2017-04-03 18:52:44 -0700

    Stack Correction

--- Stack.py
+++ Stack.py
@@ -6,13 +6,15 @@ class Stack(object):
     def push(self, data):
         self.top += 1
         print "Element %d is pushed in Stack at position %d" % (data, self.top)
-        self.stack.append(data)
+        # self.stack.append(data)
+        self.stack.insert(self.top, data)
 
     def pop(self):
         if self.top is -1:
             print "Stack is Empty"
         else:
-            print "Element %d is popped from Stack from position %d" %(self.stack[self.top], self.top)
+            print "Element %d is popped from Stack from position %d" % (self.stack[self.top], self.top)
+            del self.stack[self.top]
             self.top -= 1
 
     def traverse(self):
@@ -32,5 +34,11 @@ new_stack.push(40)
 new_stack.push(50)
 new_stack.traverse()
 new_stack.pop()
+new_stack.pop()
+new_stack.pop()
+new_stack.pop()
+new_stack.pop()
+new_stack.pop()
+new_stack.push(60)
 new_stack.traverse()
 
